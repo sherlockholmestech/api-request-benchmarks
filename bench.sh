e6354c878443455fbe1b47be683212c881be653f
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-01-31 19:02:07 +0800

    python fix

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 #This script has only been tested on the fish shell.
 . ~/.nvm/nvm.sh use --lts
-hyperfine "./rust-request/target/release/rust-request" "node ./nodejs-request/index.js" "cd bun-request && bun run index.js" "./go-request/go-request" "python3 /python-request/main.py" --warmup 20 --min-runs 10 --max-runs 10
+hyperfine "./rust-request/target/release/rust-request" "node ./nodejs-request/index.js" "cd bun-request && bun run index.js" "./go-request/go-request" "cd python-request && python3 main.py" --warmup 20 --min-runs 10 --max-runs 10
