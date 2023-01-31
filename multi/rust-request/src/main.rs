6df2f4f3eaaf24d759cf72f84c6ba813aa57dd60
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-01-31 21:55:40 +0800

    rust better

--- multi/rust-request/src/main.rs
+++ multi/rust-request/src/main.rs
@@ -1,5 +1,8 @@
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
+    getRequest();
+}
+async fn getRequest() -> Result<(), Box<dyn std::error::Error>> {
     for _ in 1..=10 {
     let resp = reqwest::get("https://fakerjs-api.sherlockholmese.repl.co/ipsium")
         .await?;
